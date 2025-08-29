3f26f305481cb6780cf9c5aa8cd9155ec0c46ed4
Bruno <bcornejo@mail.austral.edu.ar>
2025-08-29 12:29:36 -0300

    entrega

--- ada.py
+++ ada.py
@@ -2,8 +2,8 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
 
-    result1 = first_name.lower() + " " + last_name.lower()
-    result2 = first_name.title() + " " + last_name.title()
-    result3 = first_name.upper() + " " + last_name.upper()
-    result4 = "\t" + first_name.lower() + " " + last_name.lower()
+    print(first_name.lower() + " " + last_name.lower())
+    print(first_name.title() + " " + last_name.title())
+    print(first_name.upper() + " " + last_name.upper())
+    print("\t" + first_name.lower() + " " + last_name.lower())
 ada()
